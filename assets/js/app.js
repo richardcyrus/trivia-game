46fbe810e5025fceff203f5a526afd8c474c6288
Richard Cyrus <richard.cyrus@rcyrus.com>
2018-10-25 16:57:28 -0400

    Completed the functionality for when questions time out.
    Also knocked it down to a single timer function.

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,12 +19,12 @@
  *      - After a brief timeout, display the next question in the game.
  * - If the countdown timer expires:
  *      - Display a screen to notify the player that they have run out
- *        of time to answer the question.
- *      - Also display the correct answer.
- *      - After a brief timeout, display the next question in the game.
+ *        of time to answer the question. (done)
+ *      - Also display the correct answer. (done)
+ *      - After a brief timeout, display the next question in the game. (done)
  * - When all questions have been played:
- *      - Display the number of correct answers.
- *      - Display the number of incorrect answers.
+ *      - Display the number of correct answers. (done)
+ *      - Display the number of incorrect answers. (done)
  *      - Display an option to start the game again.
  *
  * Note: For all display options in the game, do not reload the page.
@@ -33,12 +33,11 @@
  * - Starting the game again if the player chooses to do so.
  */
 
-(function (window, $) {
+(function ($) {
     'use strict';
 
     /**
-     * A Fisher-Yates shuffle to ensure that for each turn of the game
-     * the word presented is unique.
+     * A Fisher-Yates shuffle.
      *
      * @see https://bost.ocks.org/mike/shuffle/
      * @see https://www.frankmitchell.org/2015/01/fisher-yates/
@@ -66,32 +65,11 @@
 
 
     const TriviaGame = {
-        /**
-         * This tracks the timer for question play.
-         */
         questionTimerId: 0,
-        /**
-         * This tracks the display change timer. Those cases where we are
-         * between questions (right or wrong answer).
-         */
         messageTimerId: 0,
-        /**
-         * The number of seconds to display the messaging between questions.
-         */
         messageTimeout: 10,
-        /**
-         * The Trivia questions for play at page load.
-         * For use when the player has exhausted all questions and chooses
-         * to play again.
-         */
         playList: [],
-        /**
-         * The Trivia questions for play since browser page load.
-         */
         currentPlayList: [],
-        /**
-         * The question object currently on the screen.
-         */
         currentQuestion: {},
 
         init: function(questions, bindEvents = false) {
@@ -113,18 +91,16 @@
         },
 
         startPlay: function() {
-            console.log('Called startPlay');
+            this.currentQuestion = this.currentPlayList.shift();
+            this.currentQuestion.choices = shuffle(this.currentQuestion.choices);
 
-            if (this.currentPlayList.length > 0) {
-                this.currentQuestion = this.currentPlayList.shift();
-                this.currentQuestion.choices = shuffle(this.currentQuestion.choices);
+            this.showQuestion(this.currentQuestion);
 
-                console.log('Pushing to the screen. ' + this.currentQuestion);
-                this.showQuestion(this.currentQuestion);
-
-                console.log('Starting a new timer');
-                this.timer(this.updateQuestionTimer, this.questionTimerId);
-            }
+            this.timer(
+                this.questionTimerId,
+                $.proxy(this.timerDecision, this),
+                this.updateQuestionTimer
+            );
         },
 
         showQuestion: function(data) {
@@ -152,35 +128,6 @@
             $('.countdown').text(seconds);
         },
 
-        outOfTimeTimer: function() {
-            clearInterval(this.messageTimerId);
-
-            const now = Date.now();
-            const then = now + this.messageTimeout * 1000;
-
-            this.messageTimerId = setInterval(() => {
-
-                const secondsLeft = Math.round((then - Date.now()) / 1000);
-
-                // Should we stop?
-                if (secondsLeft < 0) {
-
-                    // Stop setInterval.
-                    clearInterval(this.messageTimerId);
-
-                    // Must be called here. If it isn't the Time's-up
-                    // message isn't displayed.
-                    this.startPlay();
-
-                    // If this return is not here, on the last one it
-                    // will still fall through after calling this.startPlay();
-                    return;
-                }
-
-                console.log(`Time's-up message countdown: ${secondsLeft}`);
-            }, 1000);
-        },
-
         displayMessage: function() {
             $('.row').html(
                 `<div class="card">
@@ -195,58 +142,103 @@
             );
         },
 
+        displayGameStats: function () {
+            $('.row').html(
+                `<div class="card">
+                    <div class="card-header">
+                        <h1 class="card-title time-is-up">We have reached the end of the game!</h1>
+                    </div>
+                    <div class="card-body">
+                        <h2>Here's how you scored:</h2>
+                        <p class="text-center">${correctAnswerCount} Questions answered correctly.</p>
+                        <p class="text-center">${incorrectAnswerCount} Questions answered incorrectly.</p>
+                    </div>
+                </div>`
+            );
+        },
+
         timerDecision: function() {
-            console.log("Timer's run out!");
+            /**
+             * If the player ran out of time before clicking, then they
+             * do not get points for the question.
+             */
+            incorrectAnswerCount++;
+
+            // Show the player the correct answer.
+            this.displayMessage();
 
             if (this.currentPlayList.length === 0) {
-                // Show statistics. This case if the user runs out of time
-                // on the last question. Show message, show stats for
-                // game over.
-
-                this.displayMessage();
-                // this.outOfTimeTimer();
-                console.log('Game Over!');
+                /**
+                 * **Game Over!** Start a timer to display the game
+                 * statistics. This allows the player time to read the
+                 * correct answer. This will show the end of game
+                 * statistics when the timer reaches 0.
+                 */
+                this.timer(
+                    this.messageTimerId,
+                    $.proxy(this.displayGameStats, this),
+                    false,
+                    this.messageTimeout
+                );
             } else {
-                // This is the case if the user runs out of time
-                // on a question, and there are more question to play.
-                // Show message, pick question, start new question at
-                // messageTimer === 0
-                console.log(`${this.currentPlayList.length} more questions to ask.`);
-                this.displayMessage();
-                this.outOfTimeTimer();
+                /**
+                 * Start a timer to the next question. This allows the
+                 * player time to read the correct answer. This will
+                 * show the next question when the timer reaches 0.
+                 */
+                this.timer(
+                    this.messageTimerId,
+                    $.proxy(this.startPlay, this),
+                    false,
+                    this.messageTimeout
+                );
             }
         },
 
-        timer: function(displayUpdate, intervalAnchor, seconds = 15) {
-
-            // Clear existing timers.
-            clearInterval(intervalAnchor);
+        timer: function(intervalId, runWhenFinished = false, displayUpdate = false, seconds = 15) {
 
             const now = Date.now();
             const then = now + seconds * 1000;
 
-            // Start the clock.
-            displayUpdate(seconds);
+            clearInterval(intervalId);
 
-            intervalAnchor = setInterval(() => {
+            /**
+             * If provided, execute the callback provided to show the
+             * change in time remaining. This starts the clock at
+             * `seconds`.
+             */
+            if (displayUpdate) {
+                displayUpdate(seconds);
+            }
+
+            intervalId = setInterval(() => {
                 const secondsLeft = Math.round((then - Date.now()) / 1000);
 
-                // Should we stop?
                 if (secondsLeft <= 0) {
-                    // Stop setInterval.
-                    clearInterval(intervalAnchor);
+                    clearInterval(intervalId);
 
-                    // Let caller know time's up.
-                    console.log(`Reached ${secondsLeft} of ${seconds}`);
-                    this.timerDecision();
+                    /**
+                     * If provided, execute the callback `runWhenFinished`.
+                     */
+                    if (runWhenFinished) {
+                        runWhenFinished();
+                    }
 
-                    // If this return is not here, on the last one it
-                    // will still fall through after calling this.timerDecision();
+                    /**
+                     * If this return is not here, on the last one it
+                     * will still fall through. to the statement after
+                     * this block.
+                     */
                     return;
                 }
 
-                // Display time left
-                displayUpdate(secondsLeft);
+                /**
+                 * If provided, execute the callback provided to show the
+                 * change in time remaining.
+                 */
+                if (displayUpdate) {
+                    displayUpdate(secondsLeft);
+                }
             }, 1000);
         }
     };
@@ -306,4 +298,4 @@
 
     TriviaGame.init(questions);
 
-})(window, jQuery);
+})(jQuery);
