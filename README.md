274a877cba0d09cf3d05843f61661ef215a34dac
Richard Cyrus <richard.cyrus@rcyrus.com>
2018-10-27 00:29:46 -0400

    Complete Click handling and most page styling.

--- README.md
+++ README.md
@@ -20,11 +20,36 @@
     - Display the number of incorrect answers.
     - Display an option to start the game again.
 
-Note: For all display options in the game, do not reload the page.
-- This applies to the change of questions
-- The game statistics display at the end.
-- Starting the game again if the player chooses to do so.
+**Note:** For all display options in the game, do not reload the page.
+    - This applies to the change of questions
+    - The game statistics display at the end.
+    - Starting the game again if the player chooses to do so.
+
+## Notes
+
+* The inspiration for the page design is from the concept of an HTML slide show.
+* The design is based partially on work from [WebSlides](https://webslides.tv/ "WebSlides")
+* [WebSlides](https://webslides.tv/ "WebSlides") was used as a reference for some of the styles.
+
+* The game also uses a Fisher-Yates shuffle. The following locations were used as a reference.
+    - [Mike Bostock :: Fisher-Yates Shuffle](https://bost.ocks.org/mike/shuffle/)
+    - [Frank Mitchell :: The only way to shuffle an array in JavaScript](https://www.frankmitchell.org/2015/01/fisher-yates/)
+
+* The idea for the shuffle came about during the [Word Guess game](https://github.com/richardcyrus/word-guess-game). 
+    - In that game it was used to ensure that the players would exhaust the entire list of words in play, before experiencing a repeat.
+* In this game it is used to shuffle the list of questions as well as the possible answers to each question.
+    - It reduces the possibility of the questions appearing in the same order on each run of the game.
+    - It reduces the possibility of the answer choices appearing in the same order on each run of the game.
 
 # Credits
 
-The questions in this game were provided courtesy of my dear friend Ms. Rhonda Williams.
+- The questions in this game were provided courtesy of my very good friend Ms. Rhonda Williams.
+
+- The background image is from [Unsplash](https://unsplash.com "Unsplash").
+    - _Author_: [Jason Leung](https://unsplash.com/@ninjason)
+
+## The Unsplash License
+
+All photos published on Unsplash can be used for free. You can use them for commercial and noncommercial purposes. You do not need to ask permission from or provide credit to the photographer or Unsplash, although it is appreciated when possible.
+
+More precisely, Unsplash grants you an irrevocable, nonexclusive, worldwide copyright license to download, copy, modify, distribute, perform, and use photos from Unsplash for free, including for commercial purposes, without permission from or attributing the photographer or Unsplash. This license does not include the right to compile photos from Unsplash to replicate a similar or competing service.
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-/* global window, jQuery */
+/* global jQuery */
 
 /**
  * Trivia Game.
@@ -10,13 +10,13 @@
  * - Each question that is displayed to the player is accompanied by a
  *   countdown timer. (done)
  * - If the player selects the correct answer:
- *      - Display a screen congratulating the player.
- *      - After a brief timeout, display the next question in the game.
+ *      - Display a screen congratulating the player. (done)
+ *      - After a brief timeout, display the next question in the game. (done)
  * - If the player does not select the correct answer:
  *      - Display a screen to notify the player that they have chosen an
- *        incorrect answer.
- *      - Also display the correct answer.
- *      - After a brief timeout, display the next question in the game.
+ *        incorrect answer. (done)
+ *      - Also display the correct answer. (done)
+ *      - After a brief timeout, display the next question in the game. (done)
  * - If the countdown timer expires:
  *      - Display a screen to notify the player that they have run out
  *        of time to answer the question. (done)
@@ -28,9 +28,9 @@
  *      - Display an option to start the game again.
  *
  * Note: For all display options in the game, do not reload the page.
- * - This applies to the change of questions
- * - The game statistics display at the end.
- * - Starting the game again if the player chooses to do so.
+ *      - This applies to the change of questions
+ *      - The game statistics display at the end.
+ *      - Starting the game again if the player chooses to do so.
  */
 
 (function ($) {
@@ -60,18 +60,42 @@
         return list;
     }
 
+    /**
+     * Track the number of questions answered correctly.
+     *
+     * @type {number}
+     */
     let correctAnswerCount = 0;
+
+    /**
+     * Track the number of questions answered incorrectly.
+     *
+     * @type {number}
+     */
     let incorrectAnswerCount = 0;
 
+    /**
+     * A reference to the timer that is currently active. Allows us to
+     * stop the question timer when the player clicks on an answer.
+     *
+     * @type {number}
+     */
+    let timerId = 0;
 
     const TriviaGame = {
-        questionTimerId: 0,
-        messageTimerId: 0,
         messageTimeout: 10,
         playList: [],
         currentPlayList: [],
         currentQuestion: {},
+        timerElement: null,
 
+        /**
+         * Initialize the object. Get the list of questions, and whether
+         * or not to register the click handler for the questions.
+         *
+         * @param questions
+         * @param bindEvents
+         */
         init: function(questions, bindEvents = false) {
             if (this.playList.length === 0) {
                 this.playList = questions.slice();
@@ -86,10 +110,19 @@
             }
         },
 
+        /**
+         * Bind the click handler for the questions.
+         */
         bindHandler: function () {
-
+            $('.trivia__game').on(
+                'click', '.trivia__card-answer', $.proxy(this.playerClick, this)
+            );
         },
 
+        /**
+         * Pop a question off the front of the questions in play,
+         * display on screen, and start the timer.
+         */
         startPlay: function() {
             this.currentQuestion = this.currentPlayList.shift();
             this.currentQuestion.choices = shuffle(this.currentQuestion.choices);
@@ -97,66 +130,145 @@
             this.showQuestion(this.currentQuestion);
 
             this.timer(
-                this.questionTimerId,
                 $.proxy(this.timerDecision, this),
-                this.updateQuestionTimer
+                $.proxy(this.updateQuestionTimer, this)
             );
         },
 
+        /**
+         * Handle the player clicking on one of the answers.
+         *
+         * @param event
+         */
+        playerClick: function(event) {
+            const element = event.target;
+            const playerChoice = $(element).attr('data-answer');
+
+            clearInterval(timerId);
+
+            if (playerChoice === this.currentQuestion.answer) {
+                correctAnswerCount++;
+
+                // Show the player congratulations.
+                this.showCorrect();
+            } else {
+                incorrectAnswerCount++;
+
+                // Show the player the correct answer.
+                this.displayMessage();
+            }
+
+            this.handlePlay();
+        },
+
+        /**
+         * Display the question on the screen.
+         *
+         * @param data
+         */
         showQuestion: function(data) {
-            $('.row').html(
-                `<div class="card">
-                    <div class="card-header">
-                        <h5 class="card-title question">${data.question}</h5>
-                    </div>
-                    <div class="choices list-group">
-                        <div class="list-group-item border-0 flex-column align-items-start">
-                            <div class="d-flex w-100 justify-content-between">
-                                <small class="text-muted">Click on the item you think is the correct answer.</small>
-                                <small class="text-muted">Time Remaining: <span class="countdown"></span></small>
+            $('.trivia__game').html(
+                `<section class="trivia__card">
+                    <div class=trivia__card-wrap>
+                        <h1 class="trivia__question">${data.question}</h1>
+                        <hr>
+                        <div class="trivia__card-information">
+                            <ul class="trivia__card-information--sections">
+                                <li>Click to choose the correct answer.</li>
+                                <li>Time Remaining:&nbsp;<span class="countdown"></span></li>
+                            </ul>
+                        </div>
+                        <hr>
+                        <div class="trivia__card-answers">
+                            <div class="trivia__card-choices">
+                            ${data.choices.map((choice) => `<button class="trivia__card-answer" data-answer="${choice}">${choice}</button>`.trim()).join('')}
                             </div>
                         </div>
-                        ${data.choices.map((choice) => `
-                            <div class="list-group-item text-center border-0" data-answer="${data.answer === choice}">${choice}</div>
-                        `.trim()).join('')}
                     </div>
-                </div>`
+                </section>`
             );
         },
 
+        /**
+         * Display and update the time remaining.
+         *
+         * @param seconds The amount of time to display.
+         */
         updateQuestionTimer: function(seconds) {
-            $('.countdown').text(seconds);
+            /**
+             * Have to grab the element reference here since it does not
+             * exist when the page is loaded.
+             */
+            this.timerElement = $('.countdown');
+
+            if (seconds <= 10 && ! this.timerElement.hasClass('danger')) {
+                this.timerElement.addClass('danger');
+            }
+
+            this.timerElement.text(seconds);
         },
 
+        /**
+         * Display a message when the player chooses the incorrect answer
+         * or their time to choose an answer has run out.
+         */
         displayMessage: function() {
-            $('.row').html(
-                `<div class="card">
-                    <div class="card-header">
-                        <h1 class="card-title time-is-up">Your time to answer this question has expired!</h1>
+            $('.trivia__game').html(
+                `<section class="trivia__card">
+                    <div class=trivia__card-wrap>
+                        <h1 class="trivia__message">Oh-oh!</h1>
+                        <hr>
+                        <div class="trivia__wrong-answer">
+                            <p>You either chose the incorrect answer or your time expired!<p>
+                            <p>The correct answer to the question is: <i>${this.currentQuestion.answer}</i></p>
+                        </div>
                     </div>
-                    <div class="card-body">
-                        <p>The correct answer is ${this.currentQuestion.answer}</p>
-                        <p>Please wait while we choose the next question.</p>
-                    </div>
-                </div>`
+                </section>`
             );
         },
 
+        /**
+         * Display the end of play message, and tell the player how many
+         * questions they answered correctly and how many were incorrect.
+         */
         displayGameStats: function () {
-            $('.row').html(
-                `<div class="card">
-                    <div class="card-header">
-                        <h1 class="card-title time-is-up">We have reached the end of the game!</h1>
+            $('.trivia__game').html(
+                `<section class="trivia__card">
+                    <div class=trivia__card-wrap>
+                        <h1 class="trivia__game-over">We have reached the end of the game!</h1>
+                        <hr>
+                        <div class="trivia__message-details">
+                            <h2>Here's how you scored:</h2>
+                            <p>${correctAnswerCount} Questions answered correctly.</p>
+                            <p>${incorrectAnswerCount} Questions answered incorrectly.</p>
+                        </div>
                     </div>
-                    <div class="card-body">
-                        <h2>Here's how you scored:</h2>
-                        <p class="text-center">${correctAnswerCount} Questions answered correctly.</p>
-                        <p class="text-center">${incorrectAnswerCount} Questions answered incorrectly.</p>
-                    </div>
-                </div>`
+                </section>`
             );
         },
 
+        /**
+         * Display a message to the player when they have chosen the
+         * correct answer.
+         */
+        showCorrect: function() {
+            $('.trivia__game').html(
+                `<section class="trivia__card">
+                    <div class=trivia__card-wrap>
+                        <h1 class="trivia__message">Congratulations!</h1>
+                        <hr>
+                        <div class="trivia__right-answer">
+                            <p>You chose the correct answer!<p>
+                        </div>
+                    </div>
+                </section>`
+            );
+        },
+
+        /**
+         * This is fired when the player does not choose an answer before
+         * the clock has run out on the question in play.
+         */
         timerDecision: function() {
             /**
              * If the player ran out of time before clicking, then they
@@ -167,6 +279,12 @@
             // Show the player the correct answer.
             this.displayMessage();
 
+            // Continue to the next step (new question or game over).
+            this.handlePlay();
+        },
+
+        // TODO: Choose a better name for what happens here!
+        handlePlay: function() {
             if (this.currentPlayList.length === 0) {
                 /**
                  * **Game Over!** Start a timer to display the game
@@ -175,7 +293,6 @@
                  * statistics when the timer reaches 0.
                  */
                 this.timer(
-                    this.messageTimerId,
                     $.proxy(this.displayGameStats, this),
                     false,
                     this.messageTimeout
@@ -187,7 +304,6 @@
                  * show the next question when the timer reaches 0.
                  */
                 this.timer(
-                    this.messageTimerId,
                     $.proxy(this.startPlay, this),
                     false,
                     this.messageTimeout
@@ -195,12 +311,19 @@
             }
         },
 
-        timer: function(intervalId, runWhenFinished = false, displayUpdate = false, seconds = 15) {
+        /**
+         * Execute a timed delay for various events in the game.
+         *
+         * @param runWhenFinished The callback to execute when the timer expires.
+         * @param displayUpdate The callback to execute to display time remaining.
+         * @param seconds The number of seconds to count down.
+         */
+        timer: function(runWhenFinished = false, displayUpdate = false, seconds = 15) {
 
             const now = Date.now();
             const then = now + seconds * 1000;
 
-            clearInterval(intervalId);
+            clearInterval(timerId);
 
             /**
              * If provided, execute the callback provided to show the
@@ -211,11 +334,11 @@
                 displayUpdate(seconds);
             }
 
-            intervalId = setInterval(() => {
+            timerId = setInterval(() => {
                 const secondsLeft = Math.round((then - Date.now()) / 1000);
 
                 if (secondsLeft <= 0) {
-                    clearInterval(intervalId);
+                    clearInterval(timerId);
 
                     /**
                      * If provided, execute the callback `runWhenFinished`.
@@ -296,6 +419,109 @@
         }
     ];
 
-    TriviaGame.init(questions);
+    // const all_questions = [
+    //     {
+    //         question: "A whole generation of Americans refers to this famous person as 'Ole Blue Eyes'. To whom are they referring?",
+    //         choices: [
+    //             "Robert Redford",
+    //             "Chris Pine",
+    //             "Frank Sinatra",
+    //             "Bradley Cooper"
+    //         ],
+    //         answer: "Frank Sinatra"
+    //     },
+    //     {
+    //         question: "The Sex in the City Series is based on a real-life person's experiences in New York City. Name the real-life person.",
+    //         choices: [
+    //             "Carrie Bradshaw",
+    //             "Candace Bushnell",
+    //             "Miranda Priestley",
+    //             "Sarah Jessica Parker"
+    //         ],
+    //         answer: "Candace Bushnell"
+    //     },
+    //     {
+    //         question: "Cinco de Mayo was first celebrated in the United States as a show of solidarity with which country against French rule?",
+    //         choices: [
+    //             "Bolivia",
+    //             "Portugal",
+    //             "Spain",
+    //             "Mexico"
+    //         ],
+    //         answer: "Mexico"
+    //     },
+    //     {
+    //         question: "In 2008, several financial institutions faced a financial crisis, forcing the U.S. government to take measures to prevent their collapse. What phrase is used to denote the importance of those institutions?",
+    //         choices: [
+    //             "The Wolf of Wall Street",
+    //             "Too Big to Fail",
+    //             "The Big Chill",
+    //             "A Lion in Winter"
+    //         ],
+    //         answer: "Too Big to Fail"
+    //     },
+    //     {
+    //         question: "In today's lexicon, acronyms have replaced words in everyday language and messaging. What does the acronym »idk« mean?",
+    //         choices: [
+    //             "It's darn kind",
+    //             "Invention done kind",
+    //             "Internet diction kills",
+    //             "I don't know"
+    //         ],
+    //         answer: "I don't know"
+    //     },
+    //     {
+    //         question: "The Academy Awards, also known as the Oscars, are awarded annually by the Academy of Motion Picture Arts and Sciences, to recognise excellence in cinematic achievements. What is the equivalent organisation in the United Kingdom?",
+    //         choices: [
+    //             "The Socky",
+    //             "The IGOR",
+    //             "The BAFTA",
+    //             "The Tony"
+    //         ],
+    //         answer: "The BAFTA"
+    //     },
+    //     {
+    //         question: "As of January 1, 2018 how many countries are a member of the E.U.?",
+    //         choices: [
+    //             "15",
+    //             "21",
+    //             "18",
+    //             "28"
+    //         ],
+    //         answer: "28"
+    //     },
+    //     {
+    //         question: "Which country uses the dinar as their currency?",
+    //         choices: [
+    //             "Greece",
+    //             "Hungary",
+    //             "Serbia",
+    //             "Finland"
+    //         ],
+    //         answer: "Serbia"
+    //     },
+    //     {
+    //         question: "In the U.S., on average, what colour car is pulled over by the police more than any other?",
+    //         choices: [
+    //             "Red",
+    //             "Silver",
+    //             "Black",
+    //             "Grey"
+    //         ],
+    //         answer: "Red"
+    //     },
+    //     {
+    //         question: "On September 11th of which year did the United States suffer a series of terrorist attacks?",
+    //         choices: [
+    //             "2011",
+    //             "2001",
+    //             "2012",
+    //             "2013"
+    //         ],
+    //         answer: "2001"
+    //     }
+    // ];
+
+    TriviaGame.init(questions, true);
 
 })(jQuery);
